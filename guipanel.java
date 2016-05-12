96bdf3ee6c676bf96bf1a7a3bb9e14f4bbaad894
Jacob Adams <tookmund@gmail.com>
2016-05-12 09:02:57 -0400

    guipanel: Toggle wraparound and color based on state

--- guipanel.java
+++ guipanel.java
@@ -31,13 +31,23 @@ public class guipanel extends JPanel
       Object[][] l = con.toArray();
       int x = 0;
       int y = 0;
+      int n = 0;
       for(int r = 0; r < con.numRows(); r++)
       {
          x = 0;
          for (int c = 0; c < con.numColumns(); c++)
          {
-            if(l[r][c] != null) g.setColor(Color.BLACK);
-            else g.setColor(Color.WHITE);
+            n = con.getNeighbors(r,c);
+            if(l[r][c] != null) 
+            {
+               if (n > 3) g.setColor(Color.RED);
+               else g.setColor(Color.BLACK);
+            }
+            else 
+            {
+               if (n == 3) g.setColor(Color.GREEN);
+               else g.setColor(Color.WHITE);
+            }
             g.fillRect(x,y,size.width,size.height);
             x+=size.width;
          }
@@ -71,17 +81,19 @@ public class guipanel extends JPanel
             String s = JOptionPane.showInputDialog("Write to File");
             concommon.toFile(con,s);
          }
+         else if (c == 'p') con.wrap();
          // Add shift to change Y
          else if (c == '=') size.width++;
          else if (c == '+') size.height++;
          else if (c == '-') size.width--;
          else if (c == '_') size.height--;
+         
          else if (c == ' ') go = !go;
    }
    public void processmouse(Point p,int b)
    {
-      int mouseR = (int)(p.getY()/size.height);
-      int mouseC = (int)(p.getX()/size.width);
+      int mouseR = p.y/size.height;
+      int mouseC = p.x/size.width;
       if (b == 1) con.set(mouseR,mouseC,new life());
       else if (b == 3) con.set(mouseR,mouseC,null);
       System.out.println(mouseR+" "+mouseC);
