f795f5d6a1e5f9ee483715ae58ba1a74305c84f1
Jacob Adams <tookmund@gmail.com>
2016-05-26 10:14:05 -0400

    life: Remove unused code

--- life.java
+++ life.java
@@ -1,11 +1,5 @@
 public class life
 {
-   /*
-   private boolean living = false;
-   public void live () { living = true; }
-   public void die () { living = false; }
-   public boolean isLiving() { return living; } 
-   */
    public String toString()
    {
       return "*";
